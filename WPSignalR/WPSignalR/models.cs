3b85cb2c1fc06427aa3594f74870fc35831b585b
Edwin Otten <otten.nl@gmail.com>
2014-12-17 12:12:30 +0100

    added models

---
+++ WPSignalR/WPSignalR/models.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPSignalR
+{
+    class location
+    {
+        private int userId { get; set; }
+        private double latitude { get; set; }
+        private double longitude { get; set; }
+    }
+
+    class conversation
+    {
+        private int userId { get; set; }
+        private List<string> messages { get; }
+
+        public void addMessage(string message) {
+            messages.Add(message);
+        }
+    }
+}
