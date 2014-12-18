3e6e93795e10739bc66d9201ea086ab5367aa279
Edwin Otten <otten.nl@gmail.com>
2014-12-18 16:23:01 +0100

    added models to ConsoleServer

---
+++ ConsoleServer/ConsoleServer/models.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPSignalR
+{
+    public class Location
+    {
+        public Location(string userId, double latitude, double longitude)
+        {
+            this.userId = userId;
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+        public string userId { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+    }
+
+    public class Conversation
+    {
+        public Conversation(string userId)
+        {
+            this.userId = userId;
+        }
+        private string userId { get; set; }
+        private List<Message> messages { get; set; }
+
+        public void addMessage(Message message)
+        {
+            messages.Add(message);
+        }
+    }
+
+    public class Message
+    {
+        public Message(string senderId, string receiverId, string text)
+        {
+            this.senderId = senderId;
+            this.receiverId = receiverId;
+            this.text = text;
+        }
+        public string senderId { get; set; }
+        public string receiverId { get; set; }
+        public string text;
+    }
+}
