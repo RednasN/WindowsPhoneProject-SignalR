3b9a2d17f111b4fafcc1882a33b8ce2e0f5521be
Edwin Otten <otten.nl@gmail.com>
2015-01-05 12:00:23 +0100

    ClientApp making progress

---
+++ WPSignalR/WPSignalR/IConnection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPSignalR
+{
+    interface IConnection
+    {
+        void sendMessage(Message message);
+        string getMyUserId();
+
+        ConversationCollection getConversations();
+    }
+}
